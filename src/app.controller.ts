9ee86c8a2f331a7c3afb5413fc7a1dfc13ca95cd
Dirta Putra Anggara <dirtaputraanggara@gmail.com>
2020-11-18 12:51:21 +0700

    Add hello service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
+
+import { GrpcMethod } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @GrpcMethod('HelloWorldService', 'GetHelloWorld')
+  getHelloWorld() {
+    return { reply: 'Hello World!' };
   }
 }
